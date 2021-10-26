f91559b0c8e98cf67b831aa5f1f4b5c8977dfeb6
ManuelAbril2001 <manuabrilchab@gmail.com>
2021-10-26 15:32:06 -0300

    Home listo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,15 +1,15 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-})
-export class HomePage {
-
-  constructor() {}
-
-
-}
-
-
+import { Component } from '@angular/core';  
+  
+@Component({  
+  selector: 'app-home',  
+  templateUrl: 'home.page.html',  
+  styleUrls: ['home.page.scss'],  
+})  
+export class HomePage {  
+  slideOpts = {  
+    initialSlide: 1,  
+    speed: 300,  
+    effect: 'flip',  
+  };  
+  constructor() {}  
+}  
