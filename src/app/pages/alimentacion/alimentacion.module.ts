9b64010b4a256bbf6cbe28c489fa52cb4d27a568
Carlos Jesus Perez Dugarte <carlos.perez@vicentelopez.gov.ar>
2021-12-10 13:28:18 -0300

    Se realizaron cambios en la página de salud sexual

--- src/app/pages/alimentacion/alimentacion.module.ts
+++ src/app/pages/alimentacion/alimentacion.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 import { AlimentacionPageRoutingModule } from './alimentacion-routing.module';
 
@@ -17,7 +18,7 @@ import { AlimentacionPage } from './alimentacion.page';
     FormsModule,
     IonicModule,
     AlimentacionPageRoutingModule,
-    // ComponentsModule
+    ComponentsModule,
   ],
   declarations: [
     AlimentacionPage,
